e79f53256c2c845e4d367496b5eee1cd0edfba53
kemalozlp <ozlpkemal@gmail.com>
2024-10-11 21:41:33 +0300

    form objesi div içeerisine alındı signup css i oluşturuldu

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -3,12 +3,14 @@ import "./signup.css";
 export default function SignUPPage(){ 
 
     return(
-        <form action={signUp} >
+       <div className="signupContainer">
+         <form action={signUp} >
             <input type="text" name="firstName" placeholder="Adınız" /> <br />
             <input type="text" name="lastName" placeholder="Soyadınız" /> <br />
             <input type="email" name="email" placeholder="E-posta Adresiniz" /> <br />
             <input type="password" name="password" placeholder="********" />
             <button>Kayıt Ol</button>
         </form>
+       </div>
     )
 }
