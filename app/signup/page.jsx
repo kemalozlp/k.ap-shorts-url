cd2daf1e23af3795c419f695ff1f1533cf8da576
kemalozlp <ozlpkemal@gmail.com>
2024-10-12 14:11:57 +0300

    signup klasörüne labeller eklendi

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -5,9 +5,13 @@ export default function SignUPPage(){
     return(
        <div className="signupContainer">
          <form action={signUp} >
+            <label htmlFor="name"> Adınız:</label>
             <input type="text" name="firstName" placeholder="Adınız" /> <br />
+            <label htmlFor="lastName">Soyadınız:</label>
             <input type="text" name="lastName" placeholder="Soyadınız" /> <br />
+            <label htmlFor="email">E-posta Adresiniz</label>
             <input type="email" name="email" placeholder="E-posta Adresiniz" /> <br />
+            <label htmlFor="password">Şifreniz:</label>
             <input type="password" name="password" placeholder="********" />
             <button className="signupbtn">Kayıt Ol</button>
         </form>
